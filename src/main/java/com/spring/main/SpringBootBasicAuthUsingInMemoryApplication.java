3479ce4dd280a5e5d15656d2f6e12780ca946848
Pritam Karmakar <pritamkarmakar0094@gmail.com>
2020-04-29 19:08:44 +0530

    Add message

--- src/main/java/com/spring/main/SpringBootBasicAuthUsingInMemoryApplication.java
+++ src/main/java/com/spring/main/SpringBootBasicAuthUsingInMemoryApplication.java
@@ -2,12 +2,21 @@ package com.spring.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
+@RestController
 public class SpringBootBasicAuthUsingInMemoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBasicAuthUsingInMemoryApplication.class, args);
+	}
+	@GetMapping("/")
+	public String rootPage(Principal principal){
+		return "Hi " + principal.getName() + " , Welcome to Spring boot world!!";
 	}
 
 }
