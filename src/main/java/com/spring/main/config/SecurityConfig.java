7cb72991bcdb0717913fc17b6d96f7eb0bbc057a
Pritam Karmakar <pritamkarmakar0094@gmail.com>
2020-04-29 18:28:34 +0530

    Security changes

---
+++ src/main/java/com/spring/main/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.spring.main.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired PasswordEncoder encoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("dummyUser")
+                .password(encoder.encode("dummyPassword"))
+                .roles("ADMIN");
+
+    }
+
+    @Bean
+    PasswordEncoder generateEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+
+}
